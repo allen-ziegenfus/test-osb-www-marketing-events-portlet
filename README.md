56cb67b265b288c5a3b04b9bfbd579b9f1320a34
allen-ziegenfus <allen.ziegenfus@liferay.com>
2017-05-29 13:29:09 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a simple project for testing the remote services of Liferay's marketing event portlet 
+
+To get going do
+
+    npm install
+
+to get the dependencies.
+
+Then 
+
+    node index.js 
+
+will run through all the commands in the commands directory and create corresponding json results in the results directory. 
---
+++ index.js
@@ -0,0 +1,57 @@
+var fs = require("fs");
+var liferay = require("liferay-json");
+var winston = require("winston");
+var logger = new winston.Logger({
+    level: 'info',
+    transports: [
+        new(winston.transports.Console)()
+    ]
+});
+
+var config = {};
+var configDummy = {
+    server: "liferay server url",
+    user: "optional email adress of liferay user",
+    "base64auth": "optional base64 encoded email:password for authentication"
+};
+
+var configFile = "./config.json";
+try {
+    config = JSON.parse(fs.readFileSync(configFile));
+} catch (error) {
+    logger.error("Please create config file " + configFile + " with the following syntax in the current directory");
+    logger.error(JSON.stringify(configDummy) + "\n");
+    throw new Error("Could not find config file: " + configFile);
+}
+
+var invokeAndPrint = function(cmd, cb) {
+    liferay.invoke_liferay(config, cmd, function(body) {
+        logger.info(body.length);
+        logger.info(JSON.stringify(body, null, "\t"));
+        cb();
+    });
+};
+
+var minimist = require('minimist');
+var options = minimist(process.argv.slice(2));
+if (options && options.auth) {
+    if (options.auth == "false") {
+        console.log("deleting auth");
+        delete config.base64auth;
+    }
+}
+
+var paths = {
+    commands: "./commands",
+    results: "./results"
+}
+
+fs.readdir(paths.commands, function(err, command_files) {
+    command_files.forEach(function(command_file) {
+        var cmd = JSON.parse(fs.readFileSync(paths.commands + "/" + command_file));
+        logger.info("Invoking " + command_file);
+        liferay.invoke_liferay(config, cmd, function(response) {
+            fs.writeFileSync(paths.results + "/" + command_file, JSON.stringify(response, null, "\t"));
+        });
+    });
+});
